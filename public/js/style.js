fff96f82b005f671647227b90eb1878df0e4507c
Joe Hancock <joe.hancock95@gmail.com>
2019-09-16 20:04:28 -0500

    info appering on card

--- public/js/style.js
+++ public/js/style.js
@@ -1,21 +1,26 @@
 /* eslint-disable no-var */
 var textWrapper = document.querySelector('.ml1 .letters');
-textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
-anime.timeline({loop: false})
+textWrapper.innerHTML = textWrapper.textContent.replace(
+  /\S/g,
+  "<span class='letter'>$&</span>"
+);
+anime
+  .timeline({ loop: false })
   .add({
     targets: '.ml1 .letter',
-    scale: [0.3,1],
-    opacity: [0,1],
+    scale: [0.3, 1],
+    opacity: [0, 1],
     translateZ: 0,
-    easing: "easeOutExpo",
+    easing: 'easeOutExpo',
     duration: 600,
-    delay: (el, i) => 70 * (i+1)
-  }).add({
+    delay: (el, i) => 70 * (i + 1),
+  })
+  .add({
     targets: '.ml1 .line',
-    scaleX: [0,1],
-    opacity: [0.5,1],
-    easing: "easeOutExpo",
+    scaleX: [0, 1],
+    opacity: [0.5, 1],
+    easing: 'easeOutExpo',
     duration: 700,
     offset: '-=875',
-    delay: (el, i, l) => 80 * (l - i)
+    delay: (el, i, l) => 80 * (l - i),
   });
